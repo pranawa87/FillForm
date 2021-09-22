72cc676778f164827941d998dc76538a9094efbe
Pranawa Mishra <pmishra@appnexus.com>
2021-09-22 13:45:46 +0530

    path know

--- src/test/java/StepDef/Signupform_SD.java
+++ src/test/java/StepDef/Signupform_SD.java
@@ -11,6 +11,7 @@ public class Signupform_SD {
 	@Given("^User launch open signup form and submit double opt-in sign form$")
 	public void user_launch_open_signup_form_and_submit_double_opt_in_sign_form() throws Throwable {
 		SignupDoubleOptIn form = new SignupDoubleOptIn();
+		System.out.println("File will be here :-> "+System.getProperty("user.dir"));
 		String filePath = System.getProperty("user.dir")+"/src/test/java/Properties_file";
 		form.excelReadFillForm(filePath, "/testdata.xlsx", "Sheet1");
 	}
