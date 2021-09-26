c0364afbdf1f0d76f7b0ccb578285365313ee7bd
Pranawa Mishra <pmishra@appnexus.com>
2021-09-26 21:06:17 +0530

    don't submit - 26 sep_05

--- src/test/java/POM/SignupDoubleOptIn.java
+++ src/test/java/POM/SignupDoubleOptIn.java
@@ -61,7 +61,7 @@ public class SignupDoubleOptIn {
                 driver.findElement(By.cssSelector("#EMAIL")).sendKeys(emailData);
                 driver.findElement(By.cssSelector(".checkbox.checkbox_tick_positive")).click();
                 driver.findElement(By.cssSelector("button[type='submit'")).click();
-                Thread.sleep(3000);
+                Thread.sleep(1000);
                 System.out.println("Entered Company Name:  " + companyName);
                 System.out.println("Entered Email data:  " + emailData);
 
